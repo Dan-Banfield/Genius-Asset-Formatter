c1027e1fd1b0623cb58dac90a2f534e1c1ecff48
Dan Banfield <dandbanfield@gmail.com>
2022-04-20 20:51:29 +0100

    Added the ability to ask a first-time user if they want to view a tutorial or not.

---
+++ Genius Asset Formatter/Forms/TutorialForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+
+namespace Genius_Asset_Formatter
+{
+    public partial class TutorialForm : DarkForm
+    {
+        public TutorialForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Genius Asset Formatter/Program.cs
+++ Genius Asset Formatter/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Genius_Asset_Formatter
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            HandleLaunchMode();
+        }
+
+        private static void HandleLaunchMode()
+        {
+            if (Properties.Settings.Default.isFirstLaunch)
+            {
+                if (MessageBox.Show("Since this is your first launch, would you like to go through a tutorial on how to use this application?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Application.Run(new TutorialForm());
+                }
+                else
+                {
+                    Application.Run(new MainForm());
+                }
+                Properties.Settings.Default.isFirstLaunch = false;
+                Properties.Settings.Default.Save();
+                return;
+            }
             Application.Run(new MainForm());
         }
     }
