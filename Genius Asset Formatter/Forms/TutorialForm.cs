ecc71e2f01eaf4a7562f0b8564c63273148447e4
Dan Banfield <dandbanfield@gmail.com>
2022-04-20 21:22:44 +0100

    Finished the TutorialForm.

--- Genius Asset Formatter/Forms/TutorialForm.cs
+++ Genius Asset Formatter/Forms/TutorialForm.cs
@@ -40,7 +40,23 @@ namespace Genius_Asset_Formatter
                     tutorialLabel.Text = "Firstly, download any Studio Genius Japan Inc game of your choice, and play up until around chapter three (or skip through until that point).";
                     break;
                 case 2:
-                    tutorialLabel.Text = "This is the third tutorial step.";
+                    tutorialLabel.Text = "Next, open your device's file manager. If it doesn't have one, you can download a free one from the Google Play store.";
+                    break;
+                case 3:
+                    tutorialLabel.Text = "Then, navigate to the following path: Android -> data -> studio.genius.yourgamename.";
+                    tutorialImageBox.Image = Properties.Resources.Tutorial_Image_1;
+                    break;
+                case 4:
+                    tutorialLabel.Text = "Next, go from there to: files -> data -> studio.genius.yourgamename, and you should find a load of random blank files.";
+                    tutorialImageBox.Image = Properties.Resources.Tutorial_Image_2;
+                    break;
+                case 5:
+                    tutorialLabel.Text = "That's where this app comes in! With it, you can convert these files into their correct formats.";
+                    tutorialImageBox.Image = null;
+                    break;
+                case 6:
+                    tutorialLabel.Text = "Well, that's all there is to it! Thanks for using this application.";
+                    nextButton.Text = "Done";
                     break;
                 default:
                     FinishTutorial();
