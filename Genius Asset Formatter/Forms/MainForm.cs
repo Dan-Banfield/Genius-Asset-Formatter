3223b1834e81c09fb8afa13c69d007c907240b49
Dan Banfield <dandbanfield@gmail.com>
2022-04-20 20:36:57 +0100

    Set up a basic Form folder structure and created a MainForm.

--- Genius Asset Formatter/Forms/MainForm.cs
+++ Genius Asset Formatter/Forms/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Genius_Asset_Formatter
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
